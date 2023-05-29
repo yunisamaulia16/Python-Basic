1e3b39e635d74edc2d305f796ef1df08ca7022f5
yunisamaulia <yunisamaulia06@gmail.com>
2023-05-29 21:08:33 +0700

    code

--- Main.py
+++ Main.py
@@ -1,3 +1,18 @@
-print("hello world")
-nama = "Yunisa"
-print (nama)
+import time #pembuktian compile lebih cepat dari interpreter
+start_time = time.time() #pembuktian compile lebih cepat dari interpreter
+
+print ("Hello")
+print ("World")
+print ("Hello World")
+
+print ("Yunisa gadis pintar")
+#ini comment tidak akan di eksekusi
+a=10 #ini adalah comment juga (a tidak akan dieksekusi, a hanya menyimpan)
+"""ini adalah multiline comment
+ini adalah multiline comment"""
+print (a)
+
+print(time.time() - start_time, "detik") #pembuktian compile lebih cepat dari interpreter
+#kita dapat mengcompile ke yang namanya bytecode
+#Cara mengcopile buka terminal dan tuliskan 
+#python -m py_compile Main.py
