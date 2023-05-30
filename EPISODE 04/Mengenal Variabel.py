59f9d45673fef6e417bba92742adb6955a3c282c
yunisamaulia <yunisamaulia06@gmail.com>
2023-05-30 14:50:43 +0700

    code

--- EPISODE 04/Mengenal Variabel.py
+++ EPISODE 04/Mengenal Variabel.py
@@ -1,1 +1,29 @@
-print ("Hello World")
+#variabel adalah tempat menyimpan data
+
+#menaru/assigment nilai
+a = 10 #memasukan 10 ke variabel a
+x = 5
+panjang = 1000
+
+#pemanggilan pertama
+print ("Nilai a = ", a )
+print ("Nilai x = ", x )
+print ("Nilai panjang = ", panjang)
+
+#penamaan
+nilai_y = 15 #variabel tidak boleh memakai spasi,
+#sebagai pengganti gunakan undescore
+juta10 = 10000000 #ini contoh yang benar
+#penamaan varibel tidak boleh diawali dengan huruf
+nilaiZ = 17.5 #ini boleh 
+
+#pemanggilan kedua
+print ("Nilai a = ", a )
+a = 7
+print ("Nilai a = ", a )
+
+#assignment indirect
+"""(tidak langsung kepada sebuah angka, 
+tapi langsung ke variabel sebelumnya)"""
+b = a #nilai a yang di pakai, a yg terakhir
+print ("Nilai b = ", b )
