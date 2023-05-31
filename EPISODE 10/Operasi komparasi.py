54ef91d87e2a57518cfbc133ff20191d2bc4bf40
yunisamaulia <yunisamaulia06@gmail.com>
2023-05-31 18:23:43 +0700

    code

---
+++ EPISODE 10/Operasi komparasi.py
@@ -0,0 +1,46 @@
+#operasi komparasi
+
+# setiap hasil dari operasi komparasi adalah boolean (true/false)
+
+# >, <, >=, <=, ==, is, is not
+
+a = 4
+b = 2
+
+# lebih besar dari > #lebih ke posisi
+print("======lebih besar dari (>)======")
+hasil = a > 3
+print(a, '>', 3,'=', hasil)
+hasil = b > 3
+print(b, '>', 3,'=', hasil)
+hasil = b > 2
+print(b, '>', 2,'=', hasil)
+
+# kurang dari <
+print("======lebih kecil dari (<)======")
+hasil = a < 3
+print(a, '<', 3,'=', hasil)
+hasil = b < 3
+print(b, '<', 3,'=', hasil)
+hasil = b < 2
+print(b, '<', 2,'=', hasil)
+
+# lebih dari sama dengan (>=)
+print("======lebih dari sama dengan (>=)======")
+hasil = a >= 3
+print(a, '>=', 3,'=', hasil)
+hasil = b >= 3
+print(b, '>=', 3,'=', hasil)
+hasil = b >= 2
+print(b, '>=', 2,'=', hasil)
+
+# kurang dari sama dengan (<=)
+print("======kurang dari sama dengan (<=)======")
+hasil = a <= 3
+print(a, '<=', 3,'=', hasil)
+hasil = b <= 3
+print(b, '<=', 3,'=', hasil)
+hasil = b <= 2
+print(b, '<=', 2,'=', hasil)
+
+
