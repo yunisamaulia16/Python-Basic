60027cbfaa2dae22b743bdd915daf6d1419974d5
yunisamaulia <yunisamaulia06@gmail.com>
2023-05-31 15:34:17 +0700

    code

---
+++ EPISODE 09/volume dan luas bola.py
@@ -0,0 +1,8 @@
+
+jari_jari = float(input("masukan jari jari : "))
+
+volume = 4 * 3.14 * jari_jari**3
+luas = (4/3) * 3.14 * jari_jari**2
+
+print("volume bola adalah : ", volume)
+print("luas bola adalah : ", luas)
